861fc776a16b252312748be2b5e05f778690d253
RKreddy1799 <90071786+RKreddy1799@users.noreply.github.com>
2021-09-20 10:18:54 +0300

    Add files via upload

---
+++ src/command/FactoryInvoker.java
@@ -0,0 +1,29 @@
+
+package command;
+
+public class FactoryInvoker {
+    Command[] createCommands;
+    public FactoryInvoker(){
+        createCommands = new Command[7];
+        for( int z = 0; z < 7; z++) {
+            createCommands[z] = () -> {};
+        }
+    }
+    public void setCommand(int slot, Command createCommand ){
+        createCommands[slot] = createCommand;
+    }
+    public void onCreate( int slot ){
+        createCommands[slot].execute();
+    }
+    
+    public String toString(){
+        StringBuffer stringBuff = new StringBuffer();
+        stringBuff.append("\n ------- Creation process --------\n");
+        for( int i = 0; i < createCommands.length; i++ ){
+            stringBuff.append("[slot " + i + "]" + createCommands[i].getClass().getName() + "\n ");
+        }
+        
+        
+        return stringBuff.toString();
+    }
+}
---
+++ src/command/Metallic.java
@@ -0,0 +1,23 @@
+
+package command;
+
+
+public class Metallic {
+    //property
+    String name;
+    public Metallic( String name ) {
+        this.name = name;
+    }
+    public void melting(){
+        System.out.println(name + " melting materials");
+    }
+    public void forging(){
+        System.out.println(name + " forging materials");
+    }
+    public void drawing(){
+        System.out.println(name + " drawing materials");
+    }
+    public void molding(){
+        System.out.println(name + " molding materials");
+    }
+}
